6cf29b1deb65d0b929e10509b8e2e7173bdafdbf
experiencei <ayelojahighbee01@gmail.com>
2021-10-24 18:04:44 +0100

    starting the snapchat clone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { appSlice } from './features/appSlice';
 import './App.css';
+import { BrowserRouter as Router , Switch , Link , Route } from 'react-router-dom';
 import WebcamCapture from './components/webcamcapture/WebcamCapture';
 
 function App() {
   return (
     <div className="app">
-       <h2>Let build snapchat</h2>
-       <WebcamCapture />
-    </div>
+      
+      <Router>
+          <div className="app__body">
+         <Switch>
+       <Route  exact path="/">
+           <WebcamCapture />
+       </Route>
+       </Switch>
+       </div>
+   
+   </Router> 
+  </div>
   );
 }
 
