1b16f4532415b1599e205ce04533548e3d70dece
experiencei <ayelojahighbee01@gmail.com>
2021-10-24 17:22:23 +0100

    starting the snapchat clone

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="app">
        <h2>Let build snapchat</h2>
+       <WebcamCapture />
     </div>
   );
 }
---
+++ src/components/webcamcapture/WebcamCapture.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const videoConstraints = {
+    width: 250,
+    height: 400,
+    facingMode : "user",
+}
+
+
+
+function WebcamCapture() {
+    return (
+        <div className="webcamCapture">
+            
+        </div>
+    )
+}
+
+export default WebcamCapture
